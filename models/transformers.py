b94cb7a040ff9909215c2ef4da7cc4a391856488
JoshuaPlacidi <JoshuaPlacidi@gmail.com>
2022-06-07 23:05:48 +0100

    renamed files

--- models/transformers.py
+++ models/transformers.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 
+# tranformer models
+
 class transformer(nn.Module):
 	def __init__(self, input_size, hidden_size, out_size,
-				 num_heads=8, num_layers=6, dropout_p=0.0):
+				 data_src=None, num_heads=8, num_layers=6, dropout_p=0.0):
 		super(transformer, self).__init__()
 
 		self.input_size = input_size
@@ -15,7 +17,18 @@ class transformer(nn.Module):
 
 		self.sos_embedding = nn.Embedding(2, hidden_size)
 
-	def forward(self, x):
-		sos = torch.zeros(self.hidden_size)
+		self.e_to_hid = nn.Linear(8, hidden_size)
+		self.l_to_hid = nn.Linear(112, hidden_size)
+
+	def forward(self, e, l):
+		sos = self.sos_embeddings(0)
 		
+		e = self.e_to_hid(e)
+		l = self.l_to_hid(l)
+
+		x = e + l
+		x = torch.cat((sos, x), 1)
+
+		x = self.encoder(x)
+
 		return out
