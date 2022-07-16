0ef40075693bbbb6b1b6508bcafdaa822be41d07
JoshuaPlacidi <JoshuaPlacidi@gmail.com>
2022-07-16 19:48:49 +0100

    ATTN_self model implemented

--- models/transformers.py
+++ models/transformers.py
@@ -27,7 +27,7 @@ class AttentionGNN(torch.nn.Module):
 		self.param_dict = {'model_type':'ATTN_' + attention_type, 'in_dim':in_dim, 'hid_dim':hid_dim, 'out_dim':out_dim, 'layers':num_layers,
 							'dropout':dropout}
 	
-		attn_layer = TransformerConvLayer
+		attn_layer = AttentionLayer
 		
 		self.layers = torch.nn.ModuleList()
 		self.layers.append(
@@ -46,7 +46,7 @@ class AttentionGNN(torch.nn.Module):
 		self.dropout = dropout
 
 	def reset_parameters(self):
-		for layer in self.layers():
+		for layer in self.layers:
 			layer.reset_parameters()
 
 	def forward(self, batch):
@@ -54,63 +54,34 @@ class AttentionGNN(torch.nn.Module):
 			batch.x = layer(batch)
 			batch.x = F.relu(batch.x)
 			batch.x = F.dropout(batch.x, p=self.dropout, training=self.training)
-		batch.x = self.layers[-1](batch.x, batch.edge_index)
+		batch.x = self.layers[-1](batch)
 		return batch.x
 
 
-class TransformerConvLayer(MessagePassing):
+class AttentionLayer(MessagePassing):
 
 	def __init__(
 		self,
-		in_dim: Union[int, Tuple[int, int]],
+		in_dim: int,
 		out_dim: int,
 		attn_heads: int = 1,
-		concat: bool = True,
-		beta: bool = False,
-		dropout: float = 0.,
-		edge_dim: Optional[int] = None,
-		bias: bool = True,
-		root_weight: bool = True,
+		dropout: float = 0.1,
+		edge_dim: int = 8,
 		**kwargs,
 	):
 		kwargs.setdefault('aggr', 'add')
-		super(TransformerConvLayer, self).__init__(node_dim=0, **kwargs)
+		super(AttentionLayer, self).__init__(node_dim=0, **kwargs)
 
-		self.in_channels = in_dim
-		self.out_channels = out_dim
+		self.in_dim = in_dim
+		self.out_dim = out_dim
 		self.heads = attn_heads
-		self.beta = beta and root_weight
-		self.root_weight = root_weight
-		self.concat = concat
 		self.dropout = dropout
 		self.edge_dim = edge_dim
-		self._alpha = None
 
-		if isinstance(in_channels, int):
-			in_channels = (in_channels, in_channels)
-
-		self.lin_key = Linear(in_channels[0], attn_heads * out_dim)
-		self.lin_query = Linear(in_channels[1], attn_heads * out_dim)
-		self.lin_value = Linear(in_channels[0], attn_heads * out_dim)
-
-		if edge_dim is not None:
-			self.lin_edge = Linear(edge_dim, attn_heads * out_dim, bias=False)
-		else:
-			self.lin_edge = self.register_parameter('lin_edge', None)
-
-		if concat:
-			self.lin_skip = Linear(in_channels[1], attn_heads * out_dim,
-								   bias=bias)
-			if self.beta:
-				self.lin_beta = Linear(3 * attn_heads * out_dim, 1, bias=False)
-			else:
-				self.lin_beta = self.register_parameter('lin_beta', None)
-		else:
-			self.lin_skip = Linear(in_channels[1], out_dim, bias=bias)
-			if self.beta:
-				self.lin_beta = Linear(3 * out_dim, 1, bias=False)
-			else:
-				self.lin_beta = self.register_parameter('lin_beta', None)
+		self.lin_query = Linear(in_dim, attn_heads * out_dim)
+		self.lin_key = Linear(edge_dim, attn_heads * out_dim)
+		self.lin_value = Linear(in_dim, attn_heads * out_dim)
+		self.lin_skip = Linear(in_dim, attn_heads * out_dim, bias=True)
 
 		self.reset_parameters()
 
@@ -118,78 +89,49 @@ class TransformerConvLayer(MessagePassing):
 		self.lin_key.reset_parameters()
 		self.lin_query.reset_parameters()
 		self.lin_value.reset_parameters()
-		if self.edge_dim:
-			self.lin_edge.reset_parameters()
 		self.lin_skip.reset_parameters()
-		if self.beta:
-			self.lin_beta.reset_parameters()
 
 
 	def forward(self, batch):
-		x = (batch.x,batch.x)
-		edge_index = batch.edge_index
-		edge_attr = None
-		return_attention_weights = None
+		H, C = self.heads, self.out_dim
 
-		H, C = self.heads, self.out_channels
-
-		query = self.lin_query(x[0]).view(-1, H, C)
-		key = self.lin_key(x[1]).view(-1, H, C)
-		value = self.lin_value(x[1]).view(-1, H, C)
+		query = self.lin_query(batch.x).view(-1, H, C)
+		value = self.lin_value(batch.x).view(-1, H, C)
 	
-
 		# propagate_type: (query: Tensor, key:Tensor, value: Tensor, edge_attr: OptTensor) # noqa
-		out = self.propagate(edge_index, query=query, key=key, value=value,
-							 edge_attr=edge_attr, size=None)
+		x = self.propagate(batch.edge_index, query=query, edge_attr=batch.edge_attr, value=value, size=None)
 
-		alpha = self._alpha
-		self._alpha = None
+		x = x.view(-1, self.heads * self.out_dim)
 
-		if self.concat:
-			out = out.view(-1, self.heads * self.out_channels)
-		else:
-			out = out.mean(dim=1)
+		x_skip = self.lin_skip(batch.x)
 
-		if self.root_weight:
-			x_r = self.lin_skip(x[1])
-			if self.lin_beta is not None:
-				beta = self.lin_beta(torch.cat([out, x_r, out - x_r], dim=-1))
-				beta = beta.sigmoid()
-				out = beta * x_r + (1 - beta) * out
-			else:
-				out += x_r
+		x += x_skip
 
-		if isinstance(return_attention_weights, bool):
-			assert alpha is not None
-			if isinstance(edge_index, Tensor):
-				return out, (edge_index, alpha)
-			elif isinstance(edge_index, SparseTensor):
-				return out, edge_index.set_value(alpha, layout='coo')
-		else:
-			return out
+		return x
 
+	def message(self, query_i: Tensor, edge_attr: Tensor, value_j: Tensor,
+				index: Tensor, ptr: OptTensor, size_i: Optional[int]) -> Tensor:
 
-	def message(self, query_i: Tensor, query_j: Tensor, key_j: Tensor, value_j: Tensor,
-				edge_attr: OptTensor, index: Tensor, ptr: OptTensor,
-				size_i: Optional[int]) -> Tensor:
+		key = self.lin_key(edge_attr).view(-1, self.heads, self.out_dim)
+		m = self.self_attention(query=query_i, key=key, value=value_j, index=index)
 
-		if self.lin_edge is not None:
-			assert edge_attr is not None
-			edge_attr = self.lin_edge(edge_attr).view(-1, self.heads,
-													  self.out_channels)
-			key_j += edge_attr
+		return m
 
-		alpha = (query_i * key_j).sum(dim=-1) / math.sqrt(self.out_channels)
-		alpha = softmax(alpha, index, ptr, size_i)
-		self._alpha = alpha
+	def self_attention(self, query, key, value, index):
+		x = query * key
+		x = x.sum(dim=-1)
+		x = x / math.sqrt(self.out_dim)
+		
+		alpha = softmax(x, index)
 		alpha = F.dropout(alpha, p=self.dropout, training=self.training)
 
-		out = value_j
-		if edge_attr is not None:
-			out += edge_attr
+		x = value * alpha.view(-1, self.heads, 1)
+		return x
 
-		out *= alpha.view(-1, self.heads, 1)
-		return out
+	def mlp_attention(self, node_i, node_j, edge, label_j):
+		raise NotImplemented
+		# x = torch.concat([node_i, node_j, edge, label_j], dim=-1)
+		# x = 
 
 	def __repr__(self) -> str:
 		return (f'{self.__class__.__name__}({self.in_channels}, '
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import config
 from data import get_graph_data
 from models.mlp import MLP
 from models.gnn import GNN
+from models.transformers import AttentionGNN
 from training import GraphTrainer
 from logger import Logger
 
@@ -10,11 +11,13 @@ torch.manual_seed(0)
 
 
 # l = Logger()
-# l.load('logs/GCN_log.json')
+#l.load('logs/log.json')
 # l.plot_metric()
+# exit()
 # l.plot_run()
 # exit()
-# #l.plot_hyperparam_search('hyperparam_search.json')
+# l.plot_experiment_metric_curves('logs/experiment_logs.json', metric='valid_roc')
+# l.plot_experiment_comparison('logs/experiment_logs.json', metric='valid_roc')
 # exit()
 
 
@@ -36,18 +39,19 @@ criterion = torch.nn.BCEWithLogitsLoss()
 
 #model = MLP(trainer.graph.x.size(-1), 64, 112, num_layers=3, dropout=0.3)
 #model = GNN(conv_type='GCN', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
+model = AttentionGNN(attention_type='self', in_dim=8, hid_dim=64, out_dim=112)
 
-#logs = trainer.train(model.to(config.device), criterion, num_epochs=100, lr=0.01, save_log=True, num_runs=1, use_scheduler=True)
+logs = trainer.train(model.to(config.device), criterion, num_epochs=100, lr=0.01, save_log=True, num_runs=1, use_scheduler=True)
 #trainer.test(model, criterion, save_path='y_pred.pt')
 
 #param_dict = {'lr':(1e-4,1e-1), 'layers':(1,7), 'hid_dim':(32,350), 'dropout':(0,0.5)}
 #trainer.hyperparam_search(model=MLP, param_dict=param_dict, num_searches=50)
 
-mlp = MLP(in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=3, dropout=0.3)
-gcn = GNN(conv_type='GCN', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
-sage = GNN(conv_type='SAGE', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
-gat = GNN(conv_type='GAT', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
+# mlp = MLP(in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=3, dropout=0.3)
+# gcn = GNN(conv_type='GCN', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
+# sage = GNN(conv_type='SAGE', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
+# gat = GNN(conv_type='GAT', in_dim=trainer.graph.x.size(-1), hid_dim=64, out_dim=112, num_layers=1, dropout=0.1)
 
-models = [mlp, gcn, sage, gat]
+# models = [mlp, gcn, sage, gat]
 
-trainer.run_experiment(models)
+# trainer.run_experiment(models)
